b5e2a2f6c3ae719af2b7c9d01639e30feddfed71
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-10-11 18:16:34 +0300

    шахматы и треугольник

---
+++ 1_hw/Program1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace its_just_start
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            int z = Convert.ToInt32(Console.ReadLine());
+            if (x != y && z != y)
+                Console.WriteLine("0");
+            else if (x == y && y == z)
+                Console.WriteLine("3");
+            else
+                Console.WriteLine("2");
+        }
+    }
+}
---
+++ 1_hw/Program2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Class1
+{
+	class Program1()
+	{
+		static void Main(string[] args)
+	    {
+		int x1 = Convert.ToInt32(Console.ReadLine());
+		int x2 = Convert.ToInt32(Console.ReadLine());
+		int y1 = Convert.ToInt32(Console.ReadLine());
+		int y2 = Convert.ToInt32(Console.ReadLine());
+		if (x1 == y1 || y2 == x2)
+			Console.WriteLine("YES");
+		else
+			Console.WriteLine("NO");
+	}
+	}
+}
