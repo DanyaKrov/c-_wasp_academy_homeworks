59a42d3d86bb16d97d9af500c56032d58198c62c
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-10-11 19:11:34 +0300

    Мкад

---
+++ 1_hw/Program5.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace app1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int x = Convert.ToInt32(Console.ReadLine());
+			int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(x * y % 109);
+		}
+    }
+}
