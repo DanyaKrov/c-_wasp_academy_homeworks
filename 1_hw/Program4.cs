d75ce596ff63f26e5752b6850175c3c12873f970
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-10-11 19:10:37 +0300

    Шахматы 2

---
+++ 1_hw/Program4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace app1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int x1 = Convert.ToInt32(Console.ReadLine());
+			int x2 = Convert.ToInt32(Console.ReadLine());
+			int y1 = Convert.ToInt32(Console.ReadLine());
+			int y2 = Convert.ToInt32(Console.ReadLine());
+			if (x1 - y1 == -1 && x2 == y2)
+				Console.WriteLine("YES");
+			else if (x1 - y1 == 1 && x2 == y2)
+				Console.WriteLine("YES");
+			else if (x2 - y2 == -1 && x1 == y1)
+				Console.WriteLine("YES");
+			else if (x2 - y2 == 1 && x1 == y1)
+				Console.WriteLine("YES");
+			else if ((x1 - y1) * (x2 - y2) == 1)
+				Console.WriteLine("YES");
+			else if ((x1 - y1) * (x2 - y2) == -1)
+				Console.WriteLine("YES");
+			else
+				Console.WriteLine("NO");
+		}
+    }
+}
