6e7b54f478f1f054badc3584ddc8e37a09d6b1fc
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-11-11 00:44:41 +0300

    metro 1

--- Metro1/Metro.cs
+++ Metro1/Metro.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Metro1
 {
-    class Program
+    class Metro
     {
         static void Main(string[] args)
         {
